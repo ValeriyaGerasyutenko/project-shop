8fc07c075501e9353e4c9869f91ab6b70e783ec0
Anastasiya-Z <zaslavskaya.web.dev@gmail.com>
2020-03-04 02:04:51 +0200

    modal , form, validation

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -2,13 +2,35 @@
 
 let popupInit = document.querySelectorAll('.popupInit');
 let form__submit =  document.querySelectorAll('.form__submit');
+let closeBtn = document.querySelectorAll('.close-icon')
+
+function up() {
+	let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+	let t;
+	if(top > 0) {
+		window.scrollBy(0,-100);
+		t = setTimeout('up()',20);
+	} else clearTimeout(t);
+	return false;
+}
 
 popupInit.forEach(function(item){
 	item.addEventListener('click', function(){		
-		document.querySelector('body').classList.add('activeModal')
-	})
+		document.querySelector('body').classList.add('activeModal');
+		up();
+	});
+
+
 });
 
+closeBtn.forEach(function(item){
+	item.addEventListener('click', function(){		
+		document.querySelector('body').classList.remove('activeModal');
+		
+	});
+
+
+});
 
 
 form__submit.forEach(function(item){
@@ -23,25 +45,53 @@ let form = document.querySelectorAll('form');
 form.forEach(function(item){
 	/* valid name */
 	let validName = item.querySelectorAll('.validName');
+	let validPhone = item.querySelectorAll('.validPhone');
+	let validMail = item.querySelectorAll('.validMail');
 
 	validName.forEach(function(name){
 		name.addEventListener('keypress', function(){
 			console.log(this.value.match(/\w{3,}/g));
 
-			if(this.value.match(/\w{3,}/g)){
+			if(this.value.match(/[a-zA-Zа-яА-ЯёЁ']{2,}/igm)){
 				console.log('+')
 				this.classList.remove('red');
-				this.classList.add('green')
+				this.classList.add('green');
 			} else {
-				this.classList.add('red')
+				this.classList.add('red');
 			}
 
+		});
+	});
+	validPhone.forEach(function(name){
+		name.addEventListener('keypress', function(){
+			console.log(this.value.match(/\w{3,}/g));
 
+			if(this.value.match(/^(\+)?(\(\d{2,3}\) ?\d|\d)(([ \-]?\d)|( ?\(\d{2,3}\) ?)){7,12}\d$/gm)){
+				console.log('+')
+				this.classList.remove('red');
+				this.classList.add('green');
+			} else {
+				this.classList.add('red');
+			}
 
 		});
 	});
 
 
+	validMail.forEach(function(name){
+		name.addEventListener('keypress', function(){
+			console.log(this.value.match(/\w{3,}/g));
 
+			if(this.value.match(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/gim)){
+				console.log('+')
+				this.classList.remove('red');
+				this.classList.add('green');
+			} else {
+				this.classList.add('red');
+			}
+
+		});
+	});
 
 })
+
